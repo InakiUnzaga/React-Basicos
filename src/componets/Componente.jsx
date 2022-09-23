ea8d790de54b5c41253a2a88d730f5969d9365ee
inakiUnzaga <inakiunzaga88@gmail.com>
2022-09-23 17:29:00 -0300

    aprendiendo

---
+++ src/componets/Componente.jsx
@@ -0,0 +1,22 @@
+import React,{Component} from "react";
+
+
+const Componente = ( props) =>  <h2>{props.msg} </h2>
+
+
+//componente basado en funcional
+// function Componente(props){
+//     return <h2>{this.props.msg} </h2>
+// }
+
+
+// componente basado en clase
+// class  Componente extends Component {
+// render(){
+//     return <h2>{this.props.msg} </h2>
+// }
+// }
+
+
+
+export default Componente
