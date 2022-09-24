2de6a5c3924b1456e98da9f3e7342bb6699ce47d
inakiUnzaga <inakiunzaga88@gmail.com>
2022-09-24 00:45:46 -0300

    5ta

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Componente from "./componets/Componente";
+import Propiedades from "./componets/Propiedades";
 
 function App() {
   return (
@@ -21,9 +22,20 @@ function App() {
             Learn React
           </a>
         </section>
-      <section>
-        <Componente msg="Hola soy un componenete desde una props" />
-      </section>
+        <section>
+          <Componente msg="Hola soy un componenete desde una props" />
+          <hr />
+          <Propiedades
+            cadena="Esto es una cadena de texto "
+            numero={19}
+            booleano={false}
+            arreglo={[1, 2, 3]}
+            objeto={{ nombre: "inaki", correo: "inakiunzaga88@gmail.com" }}
+            funcion={(num)=>(num*num)}
+            elementoReact = {<i>esto es un elemento react</i>}
+            componenteReact= {<Componente msg="soy un componente pasado como prop"/>}
+          />
+        </section>
       </header>
     </div>
   );
--- src/componets/propiedades.jsx
+++ src/componets/propiedades.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+// npm install --save prop-types
+import PropTypes from "prop-types";
 
+// las props son un obj
 export default function Propiedades(props) {
   return (
     <div>
-      <h2>Propiedades</h2>
+      <h2>{props.porDefecto}</h2>
+      <ul>
+        <li>{props.cadena}</li>
+        <li>{props.numero}</li>
+        <li>{props.booleano ? "Verdadero" : "Falso"}</li>
+        <li>{props.arreglo.join(", ")}</li>
+        <li>{props.objeto.nombre + " - " + props.objeto.correo}</li>
+        <li>{props.arreglo.map(props.funcion).join(", ")}</li>
+        <li>{props.elementoReact}</li>
+        <li>{props.componenteReact}</li>
+      </ul>
     </div>
   );
 }
+
+Propiedades.defaultProps = {
+  porDefecto: "Las props",
+};
+
+Propiedades.propTypes = {
+  numero: PropTypes.number.isRequired,
+};
